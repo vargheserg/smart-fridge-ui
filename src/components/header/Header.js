29ea698aba1ce9a9bc01f496effa607ae92b002c
Varghese Rony George <varghese.rony.george@gmail.com>
2023-04-28 08:58:04 -0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        Smart Fridge Prototype
+        IntelliFridge Add On
         {/* <button onClick={this.getHeader}>
         Click me!
         </button> */}
